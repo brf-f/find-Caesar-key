a1605bdad86ee0075ea151f9df9bbad0d34ba1b5
LePotat0 <frotab19@Gmail.com>
2023-11-29 23:28:00 +0100

    bugfixing
    - Initial bugfixing for shell script
    
    - removed most clearly visible bugs

--- find_ceaser.sh
+++ find_ceaser.sh
@@ -1,6 +1,6 @@
-alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-getIndex(): #arr, val
+getIndex(){ #arr, val
     my_array=$1
     value=$2
 
@@ -9,8 +9,9 @@ getIndex(): #arr, val
         echo "${i}";
     fi
     done
+}
 
-decrypt(): #message, key
+decrypt(){ #message, key
 
     message=$1
     key=$2
@@ -20,34 +21,35 @@ decrypt(): #message, key
     for l in "${$message^^}"; do
         if containsElement "${$l^^}" "${alpha[@]}"; then #if the letter is actually a letter
             #find the corresponding ciphertext letter in the alphabet
-            result=$result $(alpha[($(getIndex $alpha $l) - $key) % "${#alpha[@]}"])
+            result="$result$(alpha[($(getIndex $alpha $l) - $key) % ${#alpha[@]}])"
         else
             result=$result $l
         fi
     done
 
     echo $result
-
+}
 
 # break ceaser 
-break_caesar(): #message, type, l, WORDS
+break_caesar(){ #message, type, l, WORDS
 
     message=$1
     type=$2
     l=$3
     WORDS=$4
     
-    echo $("Decrypting: "$message"\n//")
+    echo -e "Decrypting: "$message"\n ------ \n"
     
     shift=-1
-    [[ $type =  "%"]] && realWords=0 || realWords=() # 
+    [[ $type =  "%" ]] && realWords=0 || realWords=()
     total=l # 0.8 -> % | 100 -> specified
     
-    while [ ($type="%" && (${#( $message )[@]} - $realWords) > (${#( $message )[@]}*$margin) ) || ($type="x" && $shift <= l) ]; do
+    while ( [ "$type" == "%" ] && (( (${#message[@]} - ${#realWords[@]}) > (${#message[@]} * total) )) ) || ( [ "$type" == "x" ] && (( shift <= l )) ); do
+
         shift+=1
 
         if [ $type = "x" ]; then
-            realWords.append(0)
+            realWords+=(0)
         fi
         
         n_message=$(decrypt $message $shift)
@@ -59,12 +61,12 @@ break_caesar(): #message, type, l, WORDS
         for i in $n_words; do
             for l in $i; do
                 if ! containsElement "${$l^^}" "${alpha[@]}"; then
-                    i=i.replace(l, "")
+                    i=${i//$l/}
                 fi
             done
 
             if containsElement "${$i,,}" "${WORDS[@]}"; then
-                if [[ $type = "x" ]] then
+                if [[ $type = "x" ]]; then
                     realWords[c]+=1
                 else
                     realWords+=1
@@ -73,19 +75,20 @@ break_caesar(): #message, type, l, WORDS
         done
     done
 
-    [[ $type =  "x"]] && r_val=$(getIndex max(realWords) $realWords) || r_val=$shift
+    [[ $type = "x" ]] && r_val=$(getIndex max(realWords) $realWords) || r_val=$shift
 
     echo  $r_val # the most likely shift value as an integer |
-
+}
 
 # main
 
-main():
+main(){
 
     vals=$(break_caesar $1 $2 $3 $4) # 1-text (input text) \ 2-type (%, x) \ 3-l (condition: 100 words, 0.8 %) \ 4-WORDS (language wordlist)
 
-    key=$(vals[0])
-    text=$(vals[1])
+    key="${vals[0]}"
+    text="${vals[1]}"
+}
 
 # Args to add: | keys to ignore
 # ARGs: | input-text | first-match percentage (%) / best-match tries (x) | condition (100 words, 0.8 %)  | language wordlist
